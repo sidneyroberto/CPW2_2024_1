622c63f2c69b5fc8e0c26464d51d28676fe4d8d0
Sidney Sousa <sidney.sousa@ifms.edu.br>
2024-03-11 20:11:05 -0400

    adicionando exemplo03

--- exemplo03/js/index.js
+++ exemplo03/js/index.js
@@ -1,4 +1,14 @@
 const $ = document.querySelector.bind(document)
+const $all = document.querySelectorAll.bind(document)
+
+const directRed = $('#direct-red')
+const directGreen = $('#direct-green')
+const directBlue = $('#direct-blue')
+
+// No caso do Firefox, ele mantém a última seleção após a recarga da página
+directRed.checked = false
+directGreen.checked = false
+directBlue.checked = false
 
 const onChangeRangesRGBA = () => {
     const red = $('#red').value
@@ -8,9 +18,24 @@ const onChangeRangesRGBA = () => {
 
     $('#color-display-rgba').style.backgroundColor = 
         `rgba(${red}, ${green}, ${blue}, ${alpha})`
+    
+    directRed.checked = false
+    directGreen.checked = false
+    directBlue.checked = false
 }
 
-$('#red').oninput = onChangeRangesRGBA
-$('#green').oninput = onChangeRangesRGBA
-$('#blue').oninput = onChangeRangesRGBA
-$('#alpha').oninput = onChangeRangesRGBA
+$all(
+    '#color-controls-rgba input[type=range]'
+).forEach((range) => range.oninput = onChangeRangesRGBA)
+
+const changeByDirectColor = (color) => {
+    $('#color-display-rgba').style.backgroundColor = color
+    $('#red').value = 0
+    $('#green').value = 0
+    $('#blue').value = 0
+    $('#alpha').value = 1
+}
+
+directRed.onclick = () => changeByDirectColor('rgb(255, 0, 0)')
+directGreen.onclick = () => changeByDirectColor('rgb(0, 255, 0)')
+directBlue.onclick = () => changeByDirectColor('rgb(0, 0, 255)')
